ffc34f14b40bd69e7e3feee8d5f62ea4569b6061
Meka-tech <nnaemekaonyeji27@gmail.com>
2025-06-09 05:30:34 +0100

    feat:Build Main layout (Header / Footer)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Navbar from "../features/landing-page/navbar";
 import { ThreeStepExplainerSection } from "../features/landing-page/three-step-explainer";
 import { ValuePropositionSection } from "../features/landing-page/value-proposition";
+import Footer from "../features/landing-page/footer";
 
 export default function Home() {
   return (
     <main className="mx-auto w-full max-w-[1440px] bg-[#121014]">
+      <Navbar />
       <ThreeStepExplainerSection />
       <ValuePropositionSection />
+      <Footer />
     </main>
   );
 }
