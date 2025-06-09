a0ee87372bdffd0278e5faea5928f13149838d43
Abbakar Garba <abakargarbalkali@gmail.com>
2025-06-09 01:39:18 +0100

    Resolve merge conflicts

--- app/components/FAQSection.tsx
+++ app/components/FAQSection.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import FAQAccordion from "./FAQAccordion";
-import CTASection from "./CTASection";
+// import CTASection from "./CTASection";
 
 export default function FAQSection() {
   return (
-    <section className="border-2 border-blue-600 p-6 md:p-12 rounded-2xl my-12 space-y-20">
-      <div className="text-center space-y-2">
-        <h2 className="text-3xl md:text-5xl font-bold">FAQ</h2>
-        <p className="text-gray-300">Covers all the popular inquiries you may have</p>
-      </div>
+    <section className="max-w-4xl mx-auto px-4 py-12  rounded-lg shadow-md">
+      
+
       <FAQAccordion />
-      <CTASection />
+      {/* <CTASection /> */}
     </section>
   );
 }
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+// import FAQAccordion from "./components/FAQAccordion";
+// import CTASection from "./components/CTASection";
+
+// export default function Home() {
+//   return (
+//     <main className="bg-gray-900 text-white min-h-screen">
+//       <section className="max-w-4xl mx-auto px-4 py-12 border border-blue-600 rounded-lg shadow-md">
+//         <h2 className="text-3xl font-bold mb-2 text-center">FAQ</h2>
+//         <p className="text-gray-400 text-center mb-8">
+//           Covers all the popular inquiries you may have
+//         </p>
+
+//         <FAQAccordion />
+//       </section>
+
+//       <CTASection />
+//     </main>
+//   );
+// }
+
+
+
+
 import FAQSection from "./components/FAQSection";
+import CTASection from "./components/CTASection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-zinc-950 text-white px-4 md:px-8">
+    <main className="bg-gray-900 text-white min-h-screen">
       <FAQSection />
+      <CTASection />
     </main>
   );
 }
+
